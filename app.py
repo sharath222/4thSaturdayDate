30a40e1b15aa325737f491cb73b8c80c7bbfb9bc
sharath222 <68215642+sharath222@users.noreply.github.com>
2021-04-18 18:07:02 +0530

    4thSaturdayCode

---
+++ app.py
@@ -0,0 +1,99 @@
+from datetime import datetime, timedelta
+
+def validate_input(date_string):
+    """ Takes a date_string gives datetime and validates the year 
+
+            Parameters:
+                    date_string (string): date of format yyyyMMdd. Ex (20200415)
+
+    """
+    date = datetime.strptime(date_string, "%Y%m%d")
+    if date.year < 1900 or date.year > 2100:
+        return None
+    else:
+        return date
+
+
+def get_fourth_saturday(start_date, end_date):
+    """ Fetches fourth saturdays between give date range
+
+            Parameters:
+                    start_date (date): Start Date
+                    end_date (date): End Date
+
+    """
+    starting_date = validate_input(start_date)
+    ending_date = validate_input(end_date)
+    if(starting_date == None or ending_date == None):
+        print('Invalid Start and End Dates')
+        return None
+    else:
+        dates = []
+        while starting_date <= ending_date:
+            day = starting_date.isoweekday()
+            fourth_saturday = 28 - day
+            fourth_sat_date = datetime(
+                starting_date.year, starting_date.month, fourth_saturday)
+            if(fourth_sat_date > starting_date):
+                dates.append(fourth_sat_date.strftime('%Y%m%d'))
+            starting_date = get_next_date(starting_date)
+        return dates
+
+
+def get_next_date(date):
+    """ Calulate next date
+
+            Parameters:
+                    date (date): A Date
+
+    """
+    month = date.month
+    year = date.year
+    if month >= 12:
+        month = 1
+        year = year + 1
+    else:
+        month = month + 1
+    date = datetime(year, month, 1)
+    return date
+
+
+def get_saturday_mulitple_five(start_date, end_date):
+    """ Fetches all saturdays which has its date a muliple of 5
+
+            Parameters:
+                    start_date (date): Start Date
+                    end_date (date): End Date
+
+    """
+
+    starting_date = validate_input(start_date)
+    ending_date = validate_input(end_date)
+    if(starting_date == None or ending_date == None):
+        print('Invalid Start and End Dates')
+        return None
+    else:
+        dates = []
+        while starting_date <= ending_date:
+            if starting_date.isoweekday() == 6 and starting_date.day % 5 == 0:
+                dates.append(starting_date.strftime('%Y%m%d'))
+            starting_date += timedelta(days=1)
+        return dates
+
+
+start_date = input('Enter Start Date ')
+end_date = input('Enter End Date ')
+
+dates = get_fourth_saturday(start_date, end_date) + \
+    get_saturday_mulitple_five(start_date, end_date)
+
+result_dates = []
+
+# Remove duplicate items
+[result_dates.append(date) for date in dates if date not in result_dates]
+# adding start and end dates
+result_dates.insert(0, start_date)
+
+result_dates.sort()
+
+[print(date) for date in result_dates]
